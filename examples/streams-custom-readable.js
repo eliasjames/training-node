1e145ea5ca950a6df1101f4a80c0d1896a5abae0
Elias Carlston <elias@eliasjames.com>
2016-11-11 19:35:06 -0600

    Examples - change custom readable to counter

--- examples/streams-custom-readable.js
+++ examples/streams-custom-readable.js
@@ -1,15 +1,15 @@
 module.exports = function( durationMS, options ) {
   var Readable = require( 'stream' ).Readable;
   var util = require( 'util' );
-  var crypto = require('crypto');
 
-  function RandomStream( durationMS, options ) {
+  function CounterStream( durationMS, options ) {
     Readable.call( this, options );
     this.durationMS = durationMS;
+    this.counter = 0;
   }
-  util.inherits( RandomStream, Readable );
+  util.inherits( CounterStream, Readable );
 
-  RandomStream.prototype._read = function( size ) {
+  CounterStream.prototype._read = function( size ) {
     // size is API for big chunks - we can ignore it
     var ready = true;
     while ( ready ) {
@@ -18,10 +18,11 @@ module.exports = function( durationMS, options ) {
         this.push( null );
         ready = false;
       } else {
-        this.push( crypto.randomBytes( 8 ));
+        this.counter++;
+        this.push( this.counter.toString());
         this.push( '\n' );
       }
     }
   }
-  return new RandomStream( durationMS, options );
+  return new CounterStream( durationMS, options );
 }
